e36b678d9b2a985464b772d7ce1e3aa889f68992
Enrique Ramirez <eramirez.ice@ibm.com>
2023-03-12 15:48:37 -0300

    Listas enlazadas

---
+++ src/algorithm/LinkedList/Model.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Model
+    {
+        
+    }
+
+    public class intergerCell
+    {
+        public int Value;
+        public intergerCell? Next;
+    }
+}
---
+++ src/algorithm/LinkedList/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Una lista enlazada se construye a partir de objetos que a menudo se denominan celdas . 
+//La clase de la celda contiene los datos que la lista debe almacenar más un enlace a otra celda. 
+//El enlace es simplemente una referencia o puntero a otro objeto de la clase de una celda.
+
+using LinkedList;
+
+Console.WriteLine("Hello, World!");
